d32a94a8a80de9607305b412e3dbb64465550217
NTKiddd <ngotrankhanh163@gmail.com>
2022-10-26 12:56:54 +0700

    build-1.4

--- Rock Paper Scissors/main.cpp
+++ Rock Paper Scissors/main.cpp
@@ -1,2 +1,34 @@
 #include <iostream>
+#include <cstdlib>
+#include <windows.h>
 using namespace std;
+
+int playerChoice();
+
+int main()
+{
+    srand(time(NULL));
+    
+    int playerLives = 3;
+    int compLives = 3;
+    int compChoice = 1 + (rand() % 2);
+
+    cout << "Game started. \n";
+    Sleep(1500);
+    playerChoice();
+    
+    if (playerChoice() == compChoice)
+    cout << "Draw";
+
+    return 0;
+}
+
+int playerChoice()
+{
+    int choice;
+    
+    cout << "Rock(1), Paper(2), Scissors(3) \n";
+    cin >> choice;
+
+    return choice;
+}
