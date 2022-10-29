28d365baa11a6f8a5cab7a7db204f0c1e2d77080
NTKiddd <ngotrankhanh163@gmail.com>
2022-10-29 10:29:19 +0700

    build-1.7
    + some comments
    + clear screen after every turn
    + lives display

--- Rock Paper Scissors/RPS.cpp
+++ Rock Paper Scissors/RPS.cpp
@@ -11,34 +11,41 @@ void compareResult();
 bool running = true;
 int playerLives = 3;
 int compLives = 3;
+void livesAnnouce();
 
 int main()
 {   
     srand(time(NULL));
 
     cout << "Game started. \n";
-    Sleep(1500);
+    Sleep(1300);
 
     while (running)
     {
-        compChoice = 1 + (rand() % 3);
-        cout << "Computer: " << compChoice << endl;
+        system("cls");
+        compChoice = 1 + (rand() % 3);  //randomly pick up a number from 1 to 3 for compChoice
 
+        livesAnnouce();
+        
+        cout << "Computer: " << compChoice << endl;
         cout << "Rock(1), Paper(2), Scissors(3) \n";
+        cin >> playerChoice;
         
         checkValid();
 
         if (checkValid() == true)
         {
+            Sleep(500);
             compareResult();
         }
+        Sleep(2500);
     }
     return 0;
 }
 
 bool checkValid()
 {
-    cin >> playerChoice;
+    
     if (playerChoice > 3 || playerChoice < 1)
     {
         cout << "Please enter a valid value \n";
@@ -46,16 +53,15 @@ bool checkValid()
     }
     else 
     {
-        cout << "hi";
         return true;
     }
-}
+} //receive playerChoice and check if player enter a valid value (1, 2 or 3)
 
 void compareResult()
 {
     if ((playerChoice == 1 && compChoice == 3) || (playerChoice == 2 && compChoice == 1) || (playerChoice == 3 && compChoice == 2))
     {  
-    cout << "You win \n\n";
+        cout << "You win \n\n";
         compLives = compLives - 1;
     }
     else if ((playerChoice == 3 && compChoice == 1) || (playerChoice == 1 && compChoice == 2) || (playerChoice == 2 && compChoice == 3))
@@ -67,7 +73,7 @@ void compareResult()
     {
         cout << "Draw \n\n";
     }
-
+    //compare choices and announce the result
     if (playerLives == 0)
     {
         cout << "WINNER: COMPUTER";
@@ -78,4 +84,10 @@ void compareResult()
         cout << "WINNER: PLAYER";
         running = false;
     }
+}
+
+void livesAnnouce()
+{
+    cout << "Your lives: " << playerLives << "\n";
+    cout << "Computer lives: " << compLives << "\n\n";
 }
