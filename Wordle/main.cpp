173940cfa4c721a796afc0bd59b3a9ff0ab6fa37
NTKiddd <ngotrankhanh163@gmail.com>
2022-11-20 22:17:55 +0700

    wordle-2.7.0

--- Wordle/main.cpp
+++ Wordle/main.cpp
@@ -2,30 +2,36 @@
 #include <fstream>
 #include <windows.h>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    void playWordle();
     string username;
     int menuChoice;
+    ofstream userInputFlush;
 
-    cout << "ENTER YOUR USERNAME: ";
-    cin >> username;
+    userInputFlush.open("trash.txt");
+
+    //cout << "ENTER YOUR USERNAME: ";
+    //cin >> username;
     cout << "\n";
 
     cout << "Where do you want to proceed? \n";
     cout << "(1) Play Wordle \n";
-    cout << "(2) High Scores"; 
-    cin >> menuChoice;
+    cout << "(2) High Scores \n"; 
+    cin >> menuChoice; 
 
     if (menuChoice == 1)
     {
-        cout << "WORDLE";
+        cout << "WORDLE \n\n";
+        playWordle();
     }  
     else if (menuChoice == 2)
     {
-        cout << "HIGH SCORES";
+        cout << "HIGH SCORES \n\n";
     }
 
     return 0;
@@ -33,15 +39,39 @@ int main()
 
 void playWordle()
 {
-    bool playing;
+    bool playing = true;
+    int wordChoice;
+    char charFound;
+    string playerGuess;
+    string answerWord;
+    ifstream fin;
+    ofstream fout;
+    string wordList[3];
+    fin.open("wordstoguess.txt");
 
-    if (playing)
+    for (int i = 0; i < 3; i++)
     {
-        ofstream fout;
-        fout.open("wordstoguess");
+        getline(fin, wordList[i]);
+    }
 
-        if (!fout)
-            cout << "failed to open file";
+    
+    if (playing)
+    {   
+        srand(time(NULL));
+        wordChoice = 0 + (rand() % 3);
+        answerWord = wordList[wordChoice];
+        cout << answerWord << "\n";
+        cout << "_____ \n";
+        cout << "Enter your guess: \n";
+        cin >> playerGuess;
+
+        for (int j = 0; j < answerWord.size(); j++)
+        {
+            if (playerGuess.at(j) == answerWord.at(j))
+            {
+                cout << "found";
+            }
+        }
     }
 }
 
